6cfc5db754643da234dd186ca7ca15fae7cc33e6
Aurélia Roumesy <aurelia@nalyze.team>
2024-07-01 16:19:07 +0200

    Last renames / changes after new project owner

--- python_to_typescript_interfaces/tests/utils.py
+++ python_to_typescript_interfaces/tests/utils.py
@@ -3,6 +3,8 @@ import sys
 
 def get_version() -> float:
     if sys.version_info.major < 3 or sys.version_info.minor < 8:
-        raise Exception("py-ts-interfaces only supports Python 3.8 or above.")
+        raise Exception(
+            "python-to-typescript-interfaces only supports Python 3.8 or above."
+        )
 
     return float(sys.version_info.major) + (sys.version_info.minor / 10)
